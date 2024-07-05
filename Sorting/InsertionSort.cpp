1f664885681752b7e6e33a4dae36982a65244d42
ayushi pal <ayushipal06@gmail.com>
2024-07-05 17:39:51 +0530

    Add Insertion Sort implementation

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void insertion_sort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    insertion_sort(arr, n);
+
+    cout << "Sorted array is: ";
+    printArray(arr, n);
+
+    return 0;
+}
